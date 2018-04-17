8b0cfa20835d8dbf8fbb3b14ea72bec61f998506
Jerry Chen <chen.ruijie@gmail.com>
2018-04-17 21:40:31 +0800

    disable auto init pages.js

--- vendor/assets/javascripts/pages.js
+++ vendor/assets/javascripts/pages.js
@@ -2490,8 +2490,8 @@
 
 
 })(window.jQuery);
-(function($) {
-    'use strict';
-    // Initialize layouts and plugins
-    (typeof angular === 'undefined') && $.Pages.init();
-})(window.jQuery);
+// (function($) {
+//     'use strict';
+//     // Initialize layouts and plugins
+//     (typeof angular === 'undefined') && $.Pages.init();
+// })(window.jQuery);
